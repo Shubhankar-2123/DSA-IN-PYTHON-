682756a9f449a86940d2cc0421c52e669aee387e
Shubhankar-2123 <shubhankarsawant7@gmail.com>
2026-07-07 23:44:57 +0530

    Count the Digits

---
+++ COUNT_DIGITS.py
@@ -0,0 +1,16 @@
+n = 5873
+num = n
+count = 0
+while num > 0 :
+    count += 1
+    num = num//10
+
+print(count)
+
+
+
+num = n
+from math import *
+def count_digits():
+    return int(log10(num)+1)
+print(count_digits())
