8b3e3fcfab57950707419fb43e189e9aa8049fce
Shubhankar-2123 <shubhankarsawant7@gmail.com>
2026-07-10 21:04:19 +0530

    Factors of a no. Optimal Solution

--- FACTORS.py
+++ FACTORS.py
@@ -1,19 +1,34 @@
+from math import sqrt
 n = 36
 num = n 
-result = []
+
 
 # Brute Force
-def BruteForce():
+def BruteForce(num):
+    result = []
     for i in range (1,num+1):
         if num % i == 0 :
             result.append(i)
 
     print(result)
 
-def BetterSoln():
+def BetterSoln(num):
+    result = []
     for i in range(1,(num //2) +1):
         if num % i == 0 :
-                result.append(i)
+            result.append(i)
     result.append(num)
     print(result)      
 
+def OptimalSoln(num):
+    result = []
+    for i in range (1,int(sqrt(num))+1):
+        if num % i == 0 :
+            result.append(i)
+            if num // i != i:
+                result.append(num//i)
+    # result.sort()
+    print(result)
+BruteForce(num)
+BetterSoln(num)
+OptimalSoln(num)
