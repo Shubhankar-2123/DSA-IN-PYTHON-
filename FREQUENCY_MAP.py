2b7c26223a34b9d4a6be38851f673e46a9699316
Shubhankar-2123 <shubhankarsawant7@gmail.com>
2026-07-11 14:09:46 +0530

    Frequency of numbers : Method 1

---
+++ FREQUENCY_MAP.py
@@ -0,0 +1,13 @@
+nums = [5,6,7,7,1,9,111,1,1,5,1,1]
+
+
+def Method_1(nums):
+    freq_map = {}
+    for i in range (0,len(nums)):
+        if nums[i] in freq_map:
+            freq_map[nums[i]] += 1
+        else :
+            freq_map[nums[i]] = 1
+   
+    print(freq_map)
+Method_1(nums)
