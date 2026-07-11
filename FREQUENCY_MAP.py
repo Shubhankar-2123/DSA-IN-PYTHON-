1921e192b89befb220a77b3341b6f542996e4639
Shubhankar-2123 <shubhankarsawant7@gmail.com>
2026-07-11 14:13:26 +0530

    Frequency of numbers : Method 2

--- FREQUENCY_MAP.py
+++ FREQUENCY_MAP.py
@@ -1,6 +1,6 @@
 nums = [5,6,7,7,1,9,111,1,1,5,1,1]
 
-
+# Method 1 
 def Method_1(nums):
     freq_map = {}
     for i in range (0,len(nums)):
@@ -11,3 +11,11 @@ def Method_1(nums):
    
     print(freq_map)
 Method_1(nums)
+
+def Method_2(nums):
+    hash_map = {}
+    for i in range (0 , len(nums)):
+        hash_map[nums[i]] = hash_map.get(nums[i],0) + 1
+
+    print(hash_map)
+Method_2(nums)
