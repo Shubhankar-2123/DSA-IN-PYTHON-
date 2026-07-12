72784b11a5379d77a2661679a53f53288c2ae7a8
Shubhankar-2123 <shubhankarsawant7@gmail.com>
2026-07-13 00:07:49 +0530

    Character Frequency Count : Optimal Solution using hash map

--- CHARACTER_FREQUENCY_COUNT.py
+++ CHARACTER_FREQUENCY_COUNT.py
@@ -1,17 +1,27 @@
 s = "azyxyyzaaaa"
 
-q = ['d','a','y' ,'x']
+q = ['d','a','y' ,'z']
+def Using_Dict(s,q):
+    freq_count= {}
+    for x in s:
+        if x not in freq_count:
+            freq_count[x] = 1
+        else:
+            freq_count[x] += 1
 
-freq_count= {}
-for x in s:
-    if x not in freq_count:
-        freq_count[x] = 1
-    else:
-        freq_count[x] += 1
+    for x in q :
+        if x not in freq_count:
+            print(f"Occurence of {x} is 0")
+        else :
+            print(f"occurence of {x} is {freq_count[x]}")
+    print(freq_count)
 
-for x in q :
-    if x not in freq_count:
-        print(f"Occurence of {x} is 0")
-    else :
-        print(f"occurence of {x} is {freq_count[x]}")
-print(freq_count)
+def Optimal(s,q):
+    hash_map = [0] * 26
+    for x in s :
+        hash_map[ord(x)-97] += 1
+
+    for x in q :
+        print(f"{x} occures {hash_map[ord(x)-97]} times ")
+
+Optimal(s,q)
