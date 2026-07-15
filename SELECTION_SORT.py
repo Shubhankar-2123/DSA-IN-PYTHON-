624e3a2e9ab243e1e4a110e5aa6a5d909b6111f6
Shubhankar-2123 <shubhankarsawant7@gmail.com>
2026-07-15 23:29:53 +0530

    Selection Sort : Descending 2nd

--- SELECTION_SORT.py
+++ SELECTION_SORT.py
@@ -1,6 +1,6 @@
 nums = [5,7,8,4,1,6,9,2]
 
-def Selection_Sort_Ascending():
+def Selection_Sort_Ascending(nums):
     for i in range (0 , len(nums)):
         min = i
         for j in range (i+1 , len(nums)):
@@ -10,3 +10,17 @@ def Selection_Sort_Ascending():
         nums[i],nums[min]= nums[min], nums[i]
 
     print(nums)
+
+def Selection_Sort_descending(nums):
+    for i in range (0 , len(nums)):
+        max = i
+        for j in range (i+1 , len(nums)):
+            if nums[max] < nums[j]:
+                max = j
+    
+        nums[i],nums[max]= nums[max], nums[i]
+    
+    print(nums)
+
+Selection_Sort_Ascending(nums)
+Selection_Sort_descending(nums)
