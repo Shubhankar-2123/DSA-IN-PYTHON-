563b5bd2fd7e87429cfa571a27f4cab217a826ed
Shubhankar-2123 <shubhankarsawant7@gmail.com>
2026-08-04 23:51:13 +0530

    Q6. Move Zeros at the End.

---
+++ MOVE_ZEROS_TO_END.py
@@ -0,0 +1,43 @@
+nums = [1,0,2,4,3,0,0,5,1]
+
+def Move_Zeros_Myversion(nums):
+    n = len(nums)
+    i,j=0,1
+    while i < n and j < n :
+        if nums[i] == 0 and nums[j] != 0:
+            nums[i],nums[j]= nums[j],nums[i]
+            i+=1
+            j+=1
+           
+        elif nums[i]!=0:
+            i+=1
+            j+=1
+            
+        else :
+            j+=1
+
+    print(nums)
+
+
+def Move_Zeros_Optimal(nums):
+    n = len(nums)
+
+    i = 0 
+    while i < n:
+        if nums[i] ==0:
+            break
+        i+=1
+    j = i=1
+    while j < n:
+        if nums[j] != 0 :
+            nums[i],nums[j]=nums[j],nums[i]
+            i+=1
+        j+=1
+
+    print(nums)
+    
+            
+Move_Zeros_Optimal(nums)
+
+
+
